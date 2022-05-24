e89dda4575cf12c8a14e83b87b848c27aaaa7ce6
Minh Cương <senseicuong@gmail.com>
2022-05-24 15:34:49 +0700

    init commit new

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+// import Head from 'next/head'
+// import Image from 'next/image'
+import '../styles/Home.module.css'
+import App from './App'
+
+const Home: NextPage = () => {
+	return <App />
+}
+
+export default Home
